e50db92bcff7118a3b8e9eeb8ee21bee10bdb868
Karan Singh <karanrsingh2662006@gmail.com>
2023-03-19 11:17:13 +0530

    added message schema

--- src/components/BotChatBubble.tsx
+++ src/components/BotChatBubble.tsx
@@ -21,20 +21,20 @@ const BotChatBubble = ({chat}: Props) => {
 
     <div onClick={()=> setTranslatePanelOpen(translatePanelOpen?false:true)}   className=" active:bg-base-200 md:cursor-pointer chat-bubble">
      {
-         chat.message
+         chat.reply
         }
     </div>
    {translatePanelOpen && <>
     <div className='absolute chat-bubble bottom-20  border  border-primary before:!left-1  '>
      {
-        chat?.wordsinmessage?.map(({word},i)=>(
+        chat?.wordsinreply?.map(({word},i)=>(
            <span key={i} className='hover:underline'>{word + ' '}</span>
             ))
         }
     </div>
     <div className='absolute chat-bubble top-20 before:!left-2 before:!top-1 !rounded-bl-2xl  !rounded-tl-none border  border-primary'>
     {
-        chat.messageTranslation
+        chat.replyTranslation
     }
     </div>
     </>}
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,55 +1,128 @@
-
 const languages = [
-    { id: 1, name: "English" },
-    { id: 2, name: "Mandarin Chinese" },
-    { id: 3, name: "Spanish" },
-    { id: 4, name: "French" },
-    { id: 5, name: "Arabic" },
-    { id: 6, name: "German" },
-    { id: 7, name: "Japanese" },
-    { id: 8, name: "Russian" },
-    { id: 9, name: "Portuguese" },
-    { id: 10, name: "Italian" },
-  ];
-  
-  const somechats = [
-    {
-      id: 1,
-      message: "Hi good morning",
-      responder: "user",
-      isbotrespond: false,
-      language: "es",
-      createdAt: "2022-01-01T00:00:00.000Z",
-      updatedAt: "2022-01-01T00:00:00.000Z",
-      translated: null,
-    },
-    {
-      id: 2,
-      responder: "bot",
-      language: "spanish",
-      transletedto: "english",
-      message: "Lorem ipsum dolor sit amet",
-      isbotrespond: true,
-      wordsinmessage: [
-        { word: "Lorem", translation: "Lorem ipsum dolor sit amet" },
-        { word: "ipsum", translation: "Lorem ipsum dolor sit amet" },
-        { word: "dolor", translation: "Lorem ipsum dolor sit amet" },
-        { word: "sit", translation: "Lorem ipsum dolor sit amet" },
-        { word: "amet", translation: "Lorem ipsum dolor sit amet" },
-      ],
-      messageTranslation: "Lorem ipsum dolor sit amet",
-      inreplyof: 1,
-      createdAt: "2022-01-01T00:00:00.000Z",
-      updatedAt: "2022-01-01T00:00:00.000Z",
-    },
-  ];
-  
-  export type  botChat = typeof somechats[1]; 
-  export type  Chat = typeof somechats[0]; 
-  
+  { id: 1, name: "English" },
+  { id: 2, name: "Mandarin Chinese" },
+  { id: 3, name: "Spanish" },
+  { id: 4, name: "French" },
+  { id: 5, name: "Arabic" },
+  { id: 6, name: "German" },
+  { id: 7, name: "Japanese" },
+  { id: 8, name: "Russian" },
+  { id: 9, name: "Portuguese" },
+  { id: 10, name: "Italian" },
+];
 
-
-  export {
-    languages,
-    somechats
+const somechats = [
+  {
+    id: 1,
+    message: "Hi good morning",
+    isbotrespond: false,
+    inlanguage: "english",
+    replyid      : 2,
+    createdAt: "2022-01-01T00:00:00.000Z",
+  },
+  {
+    id: 2,
+    language: "spanish",
+    isbotrespond: true,
+    reply: "Lorem ipsum dolor sit amet",
+    transletedLang: "english",
+    replyTranslation: "Lorem ipsum dolor sit amet",
+    wordsinreply: [
+      {
+        id: 23,
+        inlanguage: "spanish",
+        word: "Lorem",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+      {
+        id: 22,
+        inlanguage: "spanish",
+        word: "ipsum",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+      {
+        id: 21,
+        inlanguage: "spanish",
+        word: "dolor",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+      {
+        id: 20,
+        inlanguage: "spanish",
+        word: "sit",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+      {
+        id: 19,
+        inlanguage: "spanish",
+        word: "amet",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+    ],
+    inreplyof: 1,
+    createdAt: "2022-01-01T00:00:00.000Z",
+  },
+  {
+    id: 3,
+    message: "how are you ",
+    isbotrespond: false,
+    inlanguage: "english",
+    replyid      : 4,
+    createdAt: "2022-01-01T00:00:00.000Z",
+  },
+  {
+    id: 4,
+    language: "spanish",
+    isbotrespond: true,
+    reply: "Lorem ipsum dolor sit amet",
+    transletedLang: "english",
+    replyTranslation: "Lorem ipsum dolor sit amet",
+    wordsinreply: [
+      {
+        id: 23,
+        inlanguage: "spanish",
+        word: "Lorem",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+      {
+        id: 22,
+        inlanguage: "spanish",
+        word: "ipsum",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+      {
+        id: 21,
+        inlanguage: "spanish",
+        word: "dolor",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+      {
+        id: 20,
+        inlanguage: "spanish",
+        word: "sit",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      },
+      {
+        id: 19,
+        inlanguage: "spanish",
+        word: "amet",
+        translation: "Lorem ipsum dolor sit amet",
+        translatedLang: "english",
+      }],
   }
+  
+];
+
+export type botChat = typeof somechats[1];
+export type Chat = typeof somechats[0];
+
+export { languages, somechats };
